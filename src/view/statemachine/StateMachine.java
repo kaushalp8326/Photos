a4593b9514b6305f02b76767262aea2b82168047
John Hoban <57885268+johnhoban42@users.noreply.github.com>
2020-11-17 22:17:25 -0500

    Stock photos with relative paths

--- src/view/statemachine/StateMachine.java
+++ src/view/statemachine/StateMachine.java
@@ -1,9 +1,11 @@
 package view.statemachine;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javafx.event.Event;
 import model.Album;
+import model.Picture;
 import model.User;
 
 /**
@@ -38,11 +40,6 @@ public class StateMachine {
 	 * Album viewer state
 	 */
 	protected AlbumState albumState;
-	
-	/**
-	 * Add photo dialog state
-	 */
-	protected AddPhotoState addPhotoState;
 	
 	/**
 	 * Slideshow viewer state
@@ -94,8 +91,32 @@ public class StateMachine {
 		homeState = HomeState.getInstance();
 		datePickerState = DatePickerState.getInstance();
 		albumState = AlbumState.getInstance();
-		addPhotoState = AddPhotoState.getInstance();
 		slideshowState = SlideshowState.getInstance();
+		
+		/*User u;
+		try{
+			u = new User("stock");
+		}catch(IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		u.createAlbum("stock");
+		Album a = u.getAlbums().get(0);
+		try {
+			a.addPicture(new Picture("data/crab0start1.png"));
+			a.addPicture(new Picture("data/crab15start1.png"));
+			a.addPicture(new Picture("data/crab18start1.png"));
+			a.addPicture(new Picture("data/crab39start1.png"));
+			a.addPicture(new Picture("data/crab41start1.png"));
+			a.addPicture(new Picture("data/gamma_crab.png"));
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			u.save();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}*/
 		
 		currentState = loginState;
 		loginState.enter();
